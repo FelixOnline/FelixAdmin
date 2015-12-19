1c16d6e6c9f231ec29da70f6c8f01720dc98f9e6
Philip Kent <philip.kent@me.com>
2015-12-19 08:04:57 +0000

    Show error if unable to report problem to Sentry.
    Cannot reproduce #11 so close this now until we get more info through this
    error detail.

--- helpers/ajaxRap.php
+++ helpers/ajaxRap.php
@@ -14,8 +14,8 @@ class rapAjaxHelper extends Core {
 		$app['sentry']->extra_context(array("url" => $_POST['url'], "browser" => $_SERVER['HTTP_USER_AGENT']));
 		$event_id = $app['sentry']->getIdent($app['sentry']->captureMessage($_POST['feedback']));
 
-		if ($app['sentry']->getLastError() !== null) {
-			$this->error("Your report could not be sent due to a systems error. Please try again later.", 500);
+		if($app['sentry']->getLastError() !== null) {
+			$this->error("Your report could not be sent due to a systems error. Please try again later.\n\nError detail: ".$app['sentry']->getLastError(), 500);
 		}
 
 		$this->success(array('id' => $event_id));
