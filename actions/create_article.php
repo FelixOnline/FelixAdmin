23d1df623be07457946cb5e8a5e1b7b737c0290a
Philip Kent <philip.kent@me.com>
2015-12-26 15:17:42 +0000

    Tweak the authors box when creating articles so people with only an authors role cannot remove themselves from being an author

--- actions/create_article.php
+++ actions/create_article.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace FelixOnline\Admin\Actions;
-
+xdebug_start_trace('/Users/pkent/Desktop/foo');
 class create_article extends BaseAction {
 	public function __construct($permissions) {
 		parent::__construct($permissions);
@@ -14,15 +14,29 @@ class create_article extends BaseAction {
 
 		$this->validateAccess();
 
+		$app = \FelixOnline\Core\App::getInstance();
+		$userRoles = $app['env']['session']->session['roles'];
+		$currentuser = $app['currentuser'];
+
 		$records = $this->getRecords($records);
 		$record = $records[0];
 
 		// Add author if needed
 		$authors = $record->getAuthors();
 
-		if(!$authors) {
+		$isauthor = false;
+
+		if($authors) {
+			foreach($authors as $author) {
+				if($author->getUser() == $currentuser->getUser()) {
+					$isauthor = true;
+				}
+			}
+		}
+
+		if(!$authors ||
+			(count(array_intersect($userRoles, array('sectionEditor'))) == 0 && !$isauthor)) {
 			$app = \FelixOnline\Core\App::getInstance();
-			$currentuser = $app['currentuser'];
 
 			$rec2 = new \FelixOnline\Core\ArticleAuthor();
 			$rec2->setArticle($record);
@@ -31,24 +45,20 @@ class create_article extends BaseAction {
 		}
 
 		// If the current user has a webMaster role, submit the article straight to the publication stage.
-		$app = \FelixOnline\Core\App::getInstance();
-
-		$userRoles = $app['env']['session']->session['roles'];
-
 		if (count(array_intersect($userRoles, array('seniorEditor'))) != 0 ||
 			(count(array_intersect($userRoles, array('sectionEditor'))) != 0 && count(array_intersect($currentuser->getCategories(), array($record->getCategory()))) != 0 )) {
 			// If user is a senior editor or above, or if the user is the section editor for the category this article is in, auto review and submit for publication.
 
-			$record->setReviewer($currentuser);
+			$record->setReviewedby($currentuser);
 			$record->setHidden(0);
 			$record->save();
 
-			return 'Article created and is ready for publication. To edit it, <a href="'.STANDARD_URL.'publication_queue:details/'.$record->getId().'">click here</a>.';
+			return 'Article created and is ready for publication. To edit and publish it, <a href="'.STANDARD_URL.'articles/for_publication:details/'.$record->getId().'">click here</a>.';
 		} else {
 			$record->setHidden(1);
 			$record->save();
 
-			return 'Article created as draft. To edit it, <a href="'.STANDARD_URL.'draft_articles:details/'.$record->getId().'">click here</a>. When you are ready, go to your draft article and submit it for publication.';
+			return 'Article created as draft. To edit it, <a href="'.STANDARD_URL.'articles/drafts:details/'.$record->getId().'">click here</a>. When you are ready, go to your draft article and submit it for publication.';
 		}
 	}
 }
